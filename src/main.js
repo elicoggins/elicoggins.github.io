2c5ce6558518ceb2eeec4d7068f69e53fb5c95b1
Elijah Coggins <187310469+elicoggins@users.noreply.github.com>
2026-05-13 19:32:04 -0400

    Bundle fonts locally, remove @fontsource imports

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
-import '@fontsource/newsreader/latin-300.css';
-import '@fontsource/newsreader/latin-400.css';
-import '@fontsource/newsreader/latin-400-italic.css';
-import '@fontsource/ibm-plex-mono/latin-400.css';
-import '@fontsource/ibm-plex-mono/latin-500.css';
+import './fonts.css';
 
 import './style.css';
 
